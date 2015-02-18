2c1ae4c6b5ac1de24c07c31074e839e9ff0d17d8
Amit <amit_pt@live.co.uk>
2015-02-18 15:59:15 +0000

    MetaData test class, allows data to be read and written only to JPG's.

---
+++ src/synergy/MetaData/MetaDataTest.java
@@ -0,0 +1,113 @@
+package synergy.MetaData;
+
+/**
+ * Created by Amit on 07/02/2015.
+ */
+import java.io.*;
+import java.util.List;
+
+
+import org.apache.commons.imaging.*;
+import org.apache.commons.imaging.common.IImageMetadata;
+import org.apache.commons.imaging.common.IImageMetadata.IImageMetadataItem;
+import org.apache.commons.imaging.formats.jpeg.JpegImageMetadata;
+import org.apache.commons.imaging.formats.jpeg.exif.ExifRewriter;
+import org.apache.commons.imaging.formats.tiff.TiffImageMetadata;
+import org.apache.commons.imaging.formats.tiff.constants.ExifTagConstants;
+import org.apache.commons.imaging.formats.tiff.write.TiffOutputDirectory;
+import org.apache.commons.imaging.formats.tiff.write.TiffOutputSet;
+import org.apache.commons.imaging.util.IoUtils;
+
+public class MetaDataTest {
+
+    public IImageMetadata metadata = null;
+    public String name, values;
+
+    public void getMetaData(File f) {
+        try {
+            metadata = Imaging.getMetadata(f);
+        } catch (ImageReadException | IOException e) {
+            System.out.println("File not found");
+        }
+        if (metadata instanceof JpegImageMetadata) {
+            final JpegImageMetadata jpegMetadata = (JpegImageMetadata) metadata;
+            final List<IImageMetadataItem> items = jpegMetadata.getItems();
+
+            for (int i = 0; i < items.size(); i++) {
+                final IImageMetadataItem item = items.get(i);
+                name = item.toString().substring(0, item.toString().indexOf(":"));
+                values = item.toString();
+                //Only print out 'UserComment' tag and its values
+                //Remove string to return all meta-data tags
+                if (values.contains("UserComment")) {
+                    System.out.println(values);
+                }
+            }
+        }
+    }
+    /*
+    Modified the example method from apache.commons.imagining library
+     */
+    public void changeExifMetadata(final File jpegImageFile, final File dst)
+            throws IOException, ImageReadException, ImageWriteException {
+        OutputStream os = null;
+        boolean canThrow = false;
+        try {
+            TiffOutputSet outputSet = null;
+
+            // metadata can be null if none is attached to the jpg
+            final IImageMetadata metadata = Imaging.getMetadata(jpegImageFile);
+            final JpegImageMetadata jpegMetadata = (JpegImageMetadata) metadata;
+            if (null != jpegMetadata) {
+                // exif data can be null if none is found
+                final TiffImageMetadata exif = jpegMetadata.getExif();
+
+                if (null != exif) {
+                    outputSet = exif.getOutputSet();
+                }
+            }
+
+            // if file does not contain any exif metadata, we create an empty
+            // set of exif metadata.
+            if (null == outputSet) {
+                outputSet = new TiffOutputSet();
+            }
+            {
+                // TagInfo constants often contain a description of what
+                // directories are associated with a given tag.
+                final TiffOutputDirectory exifDirectory = outputSet
+                        .getOrCreateExifDirectory();
+                //Remove old field and replace with it with a new one.
+                exifDirectory.removeField(ExifTagConstants.EXIF_TAG_USER_COMMENT);
+                exifDirectory.add(ExifTagConstants.EXIF_TAG_USER_COMMENT,"LOC:Big Room");
+
+            }
+
+            {
+
+            }
+            os = new FileOutputStream(dst);
+            os = new BufferedOutputStream(os);
+
+            new ExifRewriter().updateExifMetadataLossless(jpegImageFile, os,
+                    outputSet);
+            canThrow = true;
+        } finally {
+            IoUtils.closeQuietly(canThrow, os);
+        }
+    }
+
+    public static void main (String args[]) throws ImageWriteException, ImageReadException, IOException {
+        MetaDataTest data = new MetaDataTest();
+
+        //Original file used for modifying
+        File inputFile = new File("C:\\Users\\Amit\\Pictures\\4.jpg");
+        //Create a new output file of the modified image.
+        File outputFile = new File ("C:\\Users\\Amit\\Pictures\\output.jpg");
+
+        data.getMetaData(inputFile);
+        data.changeExifMetadata(inputFile,outputFile);
+        data.getMetaData(outputFile);
+
+    }
+}
