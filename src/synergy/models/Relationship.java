646524a8eb6849a7ed3f01978bd01cbfb49e47f4
Sari Nusier <sari@sqhive.london>
2015-03-11 14:03:10 +0000

    Added relationship model.

---
+++ src/synergy/models/Relationship.java
@@ -0,0 +1,59 @@
+package synergy.models;
+
+import com.j256.ormlite.field.DatabaseField;
+import com.j256.ormlite.table.DatabaseTable;
+
+@DatabaseTable(tableName = "relationships")
+public class Relationship {
+
+    @DatabaseField(generatedId = true, columnName = _ID)
+    private int ID = -1;
+    @DatabaseField(canBeNull = false, columnName = COLUMN_KID1)
+    private Tag kid1Tag;
+    @DatabaseField(canBeNull = false, columnName = COLUMN_KID2)
+    private Tag kid2Tag;
+    @DatabaseField(canBeNull = false, columnName = COLUMN_OCCURRENCES)
+    private int occurrences;
+
+    public static final String COLUMN_KID1 = "KID1";
+    public static final String COLUMN_KID2 = "KID2";
+    public static final String _ID = "ID";
+    public static final String COLUMN_OCCURRENCES = "Occurrences";
+
+    public Relationship(Tag kid1Tag, Tag kid2Tag) {
+        this.kid1Tag = kid1Tag;
+        this.kid2Tag = kid2Tag;
+        occurrences = 0;
+    }
+
+
+    public void save() {
+/*        try {
+            PhotoDao.getInstance().createOrUpdate(this);
+        } catch (Exception e) {
+            System.err.println(e);
+            e.printStackTrace();
+        }*/
+    }
+
+    public int getID() {
+        return ID;
+    }
+
+    public Tag getKid1() { return kid1Tag; }
+
+    public Tag getKid2() { return kid2Tag; }
+
+    public int getOccurrences() { return occurrences; }
+
+    public void setID(int ID) {
+        this.ID = ID;
+    }
+
+    public void addPair(Tag kid1Tag, Tag kid2Tag){
+        this.kid1Tag = kid1Tag;
+        this.kid2Tag = kid2Tag;
+    }
+
+
+}
