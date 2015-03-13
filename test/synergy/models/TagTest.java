e86b405a8c2541885e5a79940c11788c524a73fd
Stoica Alex <alex_stoicajr@yahoo.co.uk>
2015-03-13 12:42:03 +0000

    Testing uniqueness of tags

--- test/synergy/models/TagTest.java
+++ test/synergy/models/TagTest.java
@@ -4,9 +4,7 @@ import org.junit.Test;
 
 import java.util.List;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertNotSame;
+import static org.junit.Assert.*;
 
 /**
  * Created by alexstoick on 3/1/15.
@@ -17,6 +15,17 @@ public class TagTest extends BaseTest {
 		Tag tag = new Tag (Tag.TagType.PLACE, "main room");
 		tag.save();
 		assertNotSame (-1, tag.getID ());
+	}
+
+	@Test public void testUniqueTag() throws Exception {
+
+		Tag tag = new Tag(Tag.TagType.KID, "Sari");
+		tag.save ();
+		assertNotEquals (-1, tag.getID ());
+		assertEquals (1, tag.getID ());
+		tag = new Tag(Tag.TagType.KID, "Sari");
+		tag.save();
+		assertEquals (1, tag.getID ());
 	}
 
 	@Test public void testSuggestedTagsForString() throws Exception {
