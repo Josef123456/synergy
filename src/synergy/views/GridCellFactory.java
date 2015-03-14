18b2fecfcca05c2af092b004424dadd4dbaa1fbc
Stoica Alex <alex_stoicajr@yahoo.co.uk>
2015-03-14 14:46:10 +0000

    Cleaned up code. Makes me happy.

---
+++ src/synergy/views/GridCellFactory.java
@@ -0,0 +1,87 @@
+package synergy.views;
+
+import controlsfx.controlsfx.control.GridCell;
+import controlsfx.controlsfx.control.GridView;
+import controlsfx.controlsfx.control.cell.ImageGridCell;
+import controlsfx.impl.org.controlsfx.skin.GridViewSkin;
+import javafx.geometry.Insets;
+import javafx.scene.image.Image;
+import javafx.scene.layout.Border;
+import javafx.scene.layout.BorderStroke;
+import javafx.scene.layout.BorderStrokeStyle;
+import javafx.util.Callback;
+import synergy.models.Photo;
+
+/**
+ * Created by alexstoick on 3/14/15.
+ */
+public class GridCellFactory implements Callback<GridView<Image>, GridCell<Image>> {
+
+	private PhotoGrid photoGrid;
+	private static ImageGridCell lastSelectedCell = null;
+
+	public GridCellFactory (PhotoGrid photoGrid) {
+		this.photoGrid = photoGrid;
+	}
+
+	@Override
+	public GridCell<Image> call (GridView<Image> param) {
+		final ImageGridCell newImageCell = new ImageGridCell();
+		newImageCell.setOnMouseClicked(event -> {
+			if (event.isShiftDown()) {
+				int lastSelectedIndex = lastSelectedCell.getIndex();
+				int newlySelectedIndex = newImageCell.getIndex();
+
+				if (newlySelectedIndex < lastSelectedIndex) {
+					int aux = newlySelectedIndex;
+					newlySelectedIndex = lastSelectedIndex;
+					lastSelectedIndex = aux;
+				}
+				int iterationIndex = newlySelectedIndex;
+				Image shiftSelectedImage = PhotoGrid.getDisplayedImagesList ().get (iterationIndex);
+				Photo shiftSelectedPhoto = PhotoGrid.getPhotos ().get (iterationIndex);
+
+				System.out.println (newlySelectedIndex + " " + lastSelectedIndex);
+
+				if (PhotoGrid.getSelectedImages ().contains (shiftSelectedImage))
+					iterationIndex--;
+				while (iterationIndex >= lastSelectedIndex) {
+					shiftSelectedPhoto = PhotoGrid.getPhotos ().get (iterationIndex);
+					PhotoGrid.getSelectedPhotos ().remove (shiftSelectedPhoto);
+					PhotoGrid.getSelectedPhotos ().add (shiftSelectedPhoto);
+
+					shiftSelectedImage = PhotoGrid.getDisplayedImagesList ().get (iterationIndex);
+					PhotoGrid.getSelectedImages ().remove (shiftSelectedImage);
+					PhotoGrid.getSelectedImages ().add (shiftSelectedImage);
+					iterationIndex--;
+				}
+			} else {
+				setCellSelection(newImageCell);
+			}
+			((GridViewSkin) photoGrid.getSkin()).updateGridViewItems();
+			lastSelectedCell = newImageCell;
+		});
+		return newImageCell;
+	}
+
+	public void setCellSelection(ImageGridCell imageCell) {
+		Image selectedImage = imageCell.getItem ();
+		int selectedImageIndex = PhotoGrid.getDisplayedImagesList ().indexOf (selectedImage);
+		if (imageCell.getBorder() == null) {
+			PhotoGrid.getSelectedPhotos ().add (PhotoGrid.getPhotos ().get (selectedImageIndex));
+			PhotoGrid.getSelectedImages ().add (selectedImage);
+
+			BorderStroke[] borderStrokeArray = new BorderStroke[4];
+			for (int i = 0; i < 4; i++)
+				borderStrokeArray[i] = new BorderStroke(javafx.scene.paint.Color
+						.BLUE, BorderStrokeStyle.SOLID, null, BorderStroke.MEDIUM,
+						new Insets (-5, -5, -5, -5));
+			imageCell.setBorder(new Border (borderStrokeArray));
+		} else {
+			PhotoGrid.getSelectedPhotos ().remove (PhotoGrid.getPhotos ().get (selectedImageIndex));
+			PhotoGrid.getSelectedImages ().remove (selectedImage);
+			imageCell.setBorder(null);
+		}
+	}
+
+}
--- src/synergy/views/PhotoGrid.java
+++ src/synergy/views/PhotoGrid.java
@@ -1,21 +1,27 @@
 package synergy.views;
 
 
+import java.util.ArrayList;
+import java.util.List;
+
+import controlsfx.controlsfx.control.GridCell;
 import controlsfx.controlsfx.control.GridView;
 import controlsfx.controlsfx.control.cell.ImageGridCell;
 import controlsfx.impl.org.controlsfx.skin.GridViewSkin;
 import javafx.collections.ObservableList;
+import javafx.event.EventHandler;
 import javafx.geometry.Insets;
 import javafx.scene.image.Image;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.Border;
 import javafx.scene.layout.BorderStroke;
 import javafx.scene.layout.BorderStrokeStyle;
+import javafx.util.Callback;
 import synergy.models.Photo;
 import synergy.tasks.ThumbnailLoaderTask;
 
 import java.util.ArrayList;
 import java.util.List;
-
 
 /**
  * Created by iHack1337 on 3/2/2015.
@@ -23,14 +29,17 @@ import java.util.List;
 public class PhotoGrid extends GridView<Image> {
 
     private static ArrayList<Image> selectedImages = new ArrayList<>();
-	private static List<Photo> photos = new ArrayList<> ();
+	private static List<Photo> photos ;
 	private static ArrayList<Photo> selectedPhotos = new ArrayList<> ();
     private static GridView<Image> photosGrid;
     private static ObservableList<Image> displayedImagesList;
-    private static ImageGridCell lastSelectedCell = null;
     private TaggingArea taggingArea;
 
-    public static GridView<Image> getPhotosGrid() {
+	public static List<Photo> getPhotos () {
+		return photos;
+	}
+
+	public static GridView<Image> getPhotosGrid() {
         return photosGrid;
     }
 
@@ -52,70 +61,10 @@ public class PhotoGrid extends GridView<Image> {
         this.taggingArea = taggingArea;
         this.setItems(displayedImagesList);
 
-        this.setCellFactory(param -> {
-                    final ImageGridCell newImageCell = new ImageGridCell();
-                    newImageCell.setOnMouseClicked(event -> {
-                        if (event.isShiftDown()) {
-                            int lastSelectedIndex = lastSelectedCell.getIndex();
-                            int newlySelectedIndex = newImageCell.getIndex();
-
-	                        if (newlySelectedIndex < lastSelectedIndex) {
-		                        int aux = newlySelectedIndex;
-		                        newlySelectedIndex = lastSelectedIndex;
-		                        lastSelectedIndex = aux;
-	                        }
-	                        int iterationIndex = newlySelectedIndex;
-                            Image shiftSelectedImage = displayedImagesList.get(iterationIndex);
-	                        Photo shiftSelectedPhoto = photos.get(iterationIndex);
-
-	                        System.out.println (newlySelectedIndex + " " + lastSelectedIndex);
-
-                            if (selectedImages.contains(shiftSelectedImage))
-                                iterationIndex--;
-                            while (iterationIndex >= lastSelectedIndex) {
-                                shiftSelectedPhoto = photos.get(iterationIndex);
-                                selectedPhotos.remove(shiftSelectedPhoto);
-                                selectedPhotos.add(shiftSelectedPhoto);
-
-                                shiftSelectedImage = displayedImagesList.get(iterationIndex);
-                                selectedImages.remove(shiftSelectedImage);
-                                selectedImages.add(shiftSelectedImage);
-                                iterationIndex--;
-                            }
-                        } else {
-	                        setCellSelection(newImageCell);
-                        }
-                        ((GridViewSkin) this.getSkin()).updateGridViewItems();
-                        lastSelectedCell = newImageCell;
-                        this.taggingArea.update();
-                    });
-
-                    return newImageCell;
-                }
-        );
+        this.setCellFactory( new GridCellFactory (this));
 
         this.setCellHeight(300);
         this.setCellWidth(300);
-    }
-
-    public void setCellSelection(ImageGridCell imageCell) {
-	    Image selectedImage = imageCell.getItem ();
-	    int selectedImageIndex = displayedImagesList.indexOf (selectedImage);
-        if (imageCell.getBorder() == null) {
-	        selectedPhotos.add(photos.get(selectedImageIndex));
-            selectedImages.add(selectedImage);
-
-            BorderStroke[] borderStrokeArray = new BorderStroke[4];
-            for (int i = 0; i < 4; i++)
-                borderStrokeArray[i] = new BorderStroke(javafx.scene.paint.Color
-                        .BLUE, BorderStrokeStyle.SOLID, null, BorderStroke.MEDIUM,
-                        new Insets(-5, -5, -5, -5));
-            imageCell.setBorder(new Border(borderStrokeArray));
-        } else {
-	        selectedPhotos.remove(photos.get(selectedImageIndex));
-            selectedImages.remove (selectedImage);
-            imageCell.setBorder(null);
-        }
     }
 
     public void setGridPhotos(final List<Photo> photosToDisplay) {
