182c9aa2a75a704a5c8d140488aed1f55e05bea9
Stoica Alex <alex_stoicajr@yahoo.co.uk>
2015-03-21 11:55:34 +0000

    Fixed issue with slider not showing up. A bit of cleanup on bottom area.

--- src/synergy/views/BottomArea.java
+++ src/synergy/views/BottomArea.java
@@ -24,10 +24,8 @@ public class BottomArea extends VBox {
 
     private Button deleteBtn, zoomMinusBtn, zoomPlusBtn;
     public static Button selectBtn, deselectBtn;
-    private Label zoomLabel;
-    private HBox zoomBox, rightBox, leftBox, selectBox;
 
-    public BottomArea() {
+	public BottomArea() {
         getStyleClass().setAll("button-bar");
         initBottomArea();
         addEventHandlers();
@@ -40,12 +38,12 @@ public class BottomArea extends VBox {
         setupNodeStyle(zoomMinusBtn, "zoomMinusBtn");
         zoomMinusBtn.setMinWidth(20);
 
-        zoomLabel = new Label("Zoom");
+	    Label zoomLabel = new Label ("Zoom");
         setupNodeStyle(zoomLabel, "zoomLabel");
 
         zoomPlusBtn = new Button("+");
         setupNodeStyle(zoomPlusBtn, "zoomPlusBtn");
-        zoomPlusBtn.setMinWidth(20);
+        zoomPlusBtn.setMinWidth (20);
 
         deselectBtn = new Button("Deselect All");
         setupNodeStyle(deselectBtn, "deselectButton");
@@ -54,22 +52,22 @@ public class BottomArea extends VBox {
         setupNodeStyle(selectBtn, "selectAllButton");
 
         deleteBtn = new Button("Delete");
-        setupNodeStyle(deleteBtn, "deleteBtn");
+        setupNodeStyle (deleteBtn, "deleteBtn");
 
-        zoomBox = new HBox(5);
-        zoomBox.getChildren().addAll(zoomMinusBtn, zoomLabel, zoomPlusBtn);
-        zoomBox.setAlignment(Pos.CENTER_LEFT);
-        zoomBox.setHgrow(zoomBox, Priority.ALWAYS);
+	    HBox zoomBox = new HBox (5);
+        zoomBox.getChildren ().addAll(zoomMinusBtn, zoomLabel, zoomPlusBtn);
+        zoomBox.setAlignment (Pos.CENTER_LEFT);
+        HBox.setHgrow (zoomBox, Priority.ALWAYS);
 
-        selectBox = new HBox(1);
-        selectBox.getChildren().addAll(selectBtn, deselectBtn);
-        selectBox.setAlignment(Pos.CENTER);
-        selectBox.setHgrow(selectBox, Priority.ALWAYS);
+	    HBox selectBox = new HBox (1);
+        selectBox.getChildren ().addAll(selectBtn, deselectBtn);
+        selectBox.setAlignment (Pos.CENTER);
+        HBox.setHgrow (selectBox, Priority.ALWAYS);
 
-        rightBox = new HBox();
-        rightBox.getChildren().add(deleteBtn);
-        rightBox.setAlignment(Pos.CENTER_RIGHT);
-        rightBox.setHgrow(rightBox, Priority.ALWAYS);
+	    HBox rightBox = new HBox ();
+        rightBox.getChildren ().add(deleteBtn);
+        rightBox.setAlignment (Pos.CENTER_RIGHT);
+        HBox.setHgrow (rightBox, Priority.ALWAYS);
 
         bottomBar.getItems().addAll(zoomBox, selectBox, rightBox);
 
@@ -79,90 +77,88 @@ public class BottomArea extends VBox {
     public void addEventHandlers() {
         PhotoGrid photoGrid = PhotoGrid.getPhotosGrid();
 
-        zoomMinusBtn.setOnAction(new javafx.event.EventHandler<ActionEvent>() {
-            @Override
-            public void handle(ActionEvent event) {
-                double cellWidth = photoGrid.getCellWidth();
-                double cellHeight = photoGrid.getCellHeight();
-                if (cellWidth > 100 & cellHeight > 100) {
-                    photoGrid.setCellWidth(cellWidth - 100);
-                    photoGrid.setCellHeight(cellHeight - 100);
-                }
-            }
-        });
-
-        zoomPlusBtn.setOnAction(new javafx.event.EventHandler<ActionEvent>() {
-            @Override
-            public void handle(ActionEvent event) {
-                double cellWidth = photoGrid.getCellWidth();
-                double cellHeight = photoGrid.getCellHeight();
-                if (cellWidth < 900 & cellHeight < 900) {
-                    photoGrid.setCellWidth(cellWidth + 100);
-                    photoGrid.setCellHeight(cellHeight + 100);
-                }
-            }
-        });
-
-        selectBtn.setOnAction(new EventHandler<ActionEvent>() {
-            @Override
-            public void handle(ActionEvent event) {
-
-                /**
-                 * TODO: Fis this part with selectALL please
-                 */
-                System.out.println(SliderBar.counter);
-                PhotoGrid.getSelectedImages().clear();
-                PhotoGrid.getSelectedImages().addAll(PhotoGrid.getDisplayedImagesList());
-                PhotoGrid.getSelectedPhotos().clear();
-                PhotoGrid.getSelectedPhotos().addAll(PhotoGrid.getDisplayedPhotos());
-                while(SliderBar.counter==0){
-
-                    ((GridViewSkin) PhotoGrid.getPhotosGrid().getSkin()).updateGridViewItems();
-                    photoGrid.getTaggingArea().update();
-
-                    SliderBar.counter = PhotoGrid.getSelectedImages().size();
-                    SliderBar.showPanel.play();
-
-                }
-            }
-        });
-
-        deselectBtn.setOnAction(new EventHandler<ActionEvent>() {
-            @Override
-            public void handle(ActionEvent event) {
-                PhotoGrid.getSelectedImages().clear();
-                PhotoGrid.getSelectedPhotos().clear();
-                ((GridViewSkin) PhotoGrid.getPhotosGrid().getSkin()).updateGridViewItems();
-                photoGrid.getTaggingArea().update();
-
-                if (PhotoGrid.getSelectedImages().isEmpty() && SliderBar.counter >= 1) {
-                    SliderBar.hidePanel.play();
-                    SliderBar.counter = PhotoGrid.getSelectedImages().size();
-                }
-            }
-        });
-
-        deleteBtn.setOnAction(new EventHandler<ActionEvent>() {
-            @Override
-            public void handle(ActionEvent event) {
-                ArrayList<Photo> selectedPhotos = new ArrayList<>();
-                selectedPhotos.addAll(PhotoGrid.getSelectedPhotos());
-                ArrayList<Image> selectedImages = new ArrayList<>();
-                selectedImages.addAll(PhotoGrid.getSelectedImages());
-
-                for (int i = 0; i < selectedPhotos.size(); i++) {
-                    Photo currentPhoto = selectedPhotos.get(i);
-                    PhotoGrid.getSelectedImages().remove(selectedImages.get(i));
-                    PhotoGrid.getDisplayedImagesList().remove(selectedImages.get(i));
-                    PhotoGrid.getSelectedPhotos().remove(selectedPhotos.get(i));
-                    currentPhoto.delete();
-                }
-                ((GridViewSkin) PhotoGrid.getPhotosGrid().getSkin()).updateGridViewItems();
-            }
-        });
+	    addZoomMinusEventHandler (photoGrid);
+	    addZoomPlusEventHandler (photoGrid);
+	    addSelectAllEventHandler (photoGrid);
+	    addDeselectAllEventHandler (photoGrid);
+	    addDeleteEventHandler ();
     }
 
-    public void setupNodeStyle(Node node, String nodeName) {
+	private void addDeleteEventHandler () {
+		deleteBtn.setOnAction(event -> {
+	        ArrayList<Photo> selectedPhotos = new ArrayList<>();
+	        selectedPhotos.addAll (PhotoGrid.getSelectedPhotos ());
+	        ArrayList<Image> selectedImages = new ArrayList<>();
+	        selectedImages.addAll(PhotoGrid.getSelectedImages());
+
+	        for (int i = 0; i < selectedPhotos.size(); i++) {
+	            Photo currentPhoto = selectedPhotos.get(i);
+	            PhotoGrid.getSelectedImages().remove(selectedImages.get(i));
+	            PhotoGrid.getDisplayedImagesList().remove(selectedImages.get(i));
+	            PhotoGrid.getSelectedPhotos().remove(selectedPhotos.get(i));
+	            currentPhoto.delete();
+	        }
+	        ((GridViewSkin) PhotoGrid.getPhotosGrid().getSkin()).updateGridViewItems();
+	    });
+	}
+
+	private void addDeselectAllEventHandler (PhotoGrid photoGrid) {
+		deselectBtn.setOnAction(event -> {
+	        PhotoGrid.getSelectedImages().clear();
+	        PhotoGrid.getSelectedPhotos ().clear ();
+	        ((GridViewSkin) PhotoGrid.getPhotosGrid().getSkin()).updateGridViewItems ();
+	        photoGrid.getTaggingArea ().update ();
+
+	        if (PhotoGrid.getSelectedImages().isEmpty() && SliderBar.counter >= 1) {
+	            SliderBar.hidePanel.play();
+	            SliderBar.counter = PhotoGrid.getSelectedImages().size();
+	        }
+	    });
+	}
+
+	private void addSelectAllEventHandler (PhotoGrid photoGrid) {
+		selectBtn.setOnAction(event -> {
+	        /**
+	         * TODO: Fis this part with selectALL please
+	         */
+	        System.out.println (SliderBar.counter);
+	        PhotoGrid.getSelectedImages ().clear();
+	        PhotoGrid.getSelectedImages ().addAll(PhotoGrid.getDisplayedImagesList ());
+	        PhotoGrid.getSelectedPhotos().clear ();
+	        PhotoGrid.getSelectedPhotos ().addAll (PhotoGrid.getDisplayedPhotos ());
+	        while(SliderBar.counter==0){
+	            ((GridViewSkin) PhotoGrid.getPhotosGrid().getSkin()).updateGridViewItems();
+	            photoGrid.getTaggingArea().update();
+
+	            SliderBar.counter = PhotoGrid.getSelectedImages().size();
+	            SliderBar.showPanel.play();
+	        }
+	    });
+	}
+
+	private void addZoomPlusEventHandler (PhotoGrid photoGrid) {
+		zoomPlusBtn.setOnAction(event -> {
+	        double cellWidth = photoGrid.getCellWidth();
+	        double cellHeight = photoGrid.getCellHeight();
+	        if (cellWidth < 900 & cellHeight < 900) {
+	            photoGrid.setCellWidth(cellWidth + 100);
+	            photoGrid.setCellHeight(cellHeight + 100);
+	        }
+	    });
+	}
+
+	private void addZoomMinusEventHandler (PhotoGrid photoGrid) {
+		zoomMinusBtn.setOnAction(event -> {
+		    double cellWidth = photoGrid.getCellWidth();
+		    double cellHeight = photoGrid.getCellHeight();
+		    if (cellWidth > 100 & cellHeight > 100) {
+		        photoGrid.setCellWidth(cellWidth - 100);
+		        photoGrid.setCellHeight(cellHeight - 100);
+		    }
+		});
+	}
+
+	public void setupNodeStyle(Node node, String nodeName) {
         node.setStyle("-fx-text-fill: antiquewhite");
         node.getStyleClass().add(nodeName);
         if (node.getClass().equals(Button.class))
