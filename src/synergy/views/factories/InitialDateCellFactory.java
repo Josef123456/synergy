e682e5d8abc20e33919e82e8bcd8c747dc1086c5
Stoica Alex <alex_stoicajr@yahoo.co.uk>
2015-03-22 17:39:51 +0000

    Ironed out some inefficiencies

--- src/synergy/views/factories/InitialDateCellFactory.java
+++ src/synergy/views/factories/InitialDateCellFactory.java
@@ -7,13 +7,13 @@ import synergy.models.Photo;
 
 import java.text.SimpleDateFormat;
 import java.time.LocalDate;
+import java.util.Date;
+import java.util.List;
 
 /**
  * Created by alexstoick on 3/21/15.
  */
 public class InitialDateCellFactory extends BaseDateCellFactory implements Callback<DatePicker, DateCell> {
-
-
 	@Override
 	public DateCell call (final DatePicker datePicker) {
 		return new DateCell () {
@@ -21,15 +21,12 @@ public class InitialDateCellFactory extends BaseDateCellFactory implements Callb
 			public void updateItem (LocalDate item, boolean empty) {
 				super.updateItem (item, empty);
 				setMinSize (50, 50);
-				for ( int i = 0 ; i < Photo.getUniqueDates ().size () ; i++ ) {
-					if ( formatDate (item).equals (new SimpleDateFormat ("dd/MM/yyyy").format (Photo.getUniqueDates ().get (i))) ) {
+				for ( int i = 0 ; i < uniqueDates.size () ; i++ ) {
+					if ( formatDate (item).equals (new SimpleDateFormat ("dd/MM/yyyy").format (uniqueDates.get (i))) ) {
 						setStyle ("-fx-background-color: #00c0cb;");
 					}
 				}
-
 			}
 		};
 	}
-
-
 }
