d97c8da9de3bde18caced29279d4c292b0703306
cham1 <k1333495@kcl.ac.uk>
2015-03-23 17:12:01 +0000

    Made the datecell factory more efficient, will try to make it even more efficient

--- src/synergy/views/factories/EndDateCellFactory.java
+++ src/synergy/views/factories/EndDateCellFactory.java
@@ -7,6 +7,8 @@ import synergy.models.Photo;
 
 import java.text.SimpleDateFormat;
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.Date;
 
 /**
  * Created by alexstoick on 3/21/15.
@@ -25,8 +27,9 @@ public class EndDateCellFactory extends BaseDateCellFactory implements Callback<
 			public void updateItem(LocalDate item, boolean empty) {
 				super.updateItem(item, empty);
 				setMinSize(50, 50);
-				for ( int i = 0 ; i < Photo.getUniqueDates().size() ; i++ ) {
-					if ( formatDate (item).equals (new SimpleDateFormat ("dd/MM/yyyy").format (Photo.getUniqueDates().get(i))) ) {
+				ArrayList<Date> uniqueDates = Photo.getUniqueDates();
+				for ( int i = 0 ; i < uniqueDates.size() ; i++ ) {
+					if ( formatDate (item).equals (new SimpleDateFormat ("dd/MM/yyyy").format(uniqueDates.get(i))) ) {
 						setStyle ("-fx-background-color: #00c0cb;");
 					}
 				}
